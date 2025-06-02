892a6a05c35646a910941371347046f3171704f9
aiueo <aiueo@aaa.com>
2025-06-02 14:26:51 +0900

    first commit

---
+++ spring-kaihatu1-login/src/main/java/com/example/demo/SpringKaihatu1LoginApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKaihatu1LoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKaihatu1LoginApplication.class, args);
+	}
+
+}
