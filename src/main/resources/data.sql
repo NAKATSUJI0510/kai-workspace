8a283a33616b6b88a35abb3c2d42e8d21ce44655
abc <abc@aaa.com>
2025-06-02 17:45:25 +0900

    DB commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,30 @@
+-- 管理者テーブルデータ
+INSERT INTO admin(id, password) VALUES(00, himitu);
+-- 学部テーブルデータ
+INSERT INTO categories(name) VALUES('文学部');
+INSERT INTO categories(name) VALUES('教育学部');
+INSERT INTO categories(name) VALUES('法学部');
+INSERT INTO categories(name) VALUES('社会学部');
+INSERT INTO categories(name) VALUES('経済学部');
+INSERT INTO categories(name) VALUES('理学部');
+INSERT INTO categories(name) VALUES('医学部');
+INSERT INTO categories(name) VALUES('歯学部');
+INSERT INTO categories(name) VALUES('薬学部');
+INSERT INTO categories(name) VALUES('工学部');
+INSERT INTO categories(name) VALUES('農学部');
+-- 商品状態テーブルデータ
+INSERT INTO states(name) VALUES('中古 (A)');
+INSERT INTO states(name) VALUES('中古 (B)');
+INSERT INTO states(name) VALUES('中古 (C)');
+-- 書籍テーブルデータ
+INSERT INTO texts(category_id, book, name, price, isbn, states) VALUES(0, '文学作品から見る社会情勢', '小川小五郎', 2000, 7210289219502);
+INSERT INTO texts(category_id, book, name, price, isbn, states) VALUES(1, '幼児期の人間関係構築 基礎', '及川実' ,1500, 8272303533140);
+INSERT INTO texts(category_id, book, name, price, isbn, states) VALUES(2, 'ポケット六法全書', '佐伯仁志， 大村敦志， 荒木尚志', 2200, 9784641009240);
+INSERT INTO texts(category_id, book, name, price, isbn, states) VALUES(3, 'メディア学基礎', '佐藤慎太郎', 1020, 2846813853126);
+INSERT INTO texts(category_id, book, name, price, isbn, states) VALUES(4, '統計学のすべて', '田中洋一', 1000, 3702072383205);
+INSERT INTO texts(category_id, book, name, price, isbn, states) VALUES(5, '数学基礎', '高橋一郎', 1300, 6066719400254);
+INSERT INTO texts(category_id, book, name, price, isbn, states) VALUES(6, '人体構造基礎', '青木繁', 1500, 7891866691382);
+INSERT INTO texts(category_id, book, name, price, isbn, states) VALUES(7, '口腔と健康', '大野義之', 1500, 3896865673774);
+INSERT INTO texts(category_id, book, name, price, isbn, states) VALUES(8, '毒と薬の世界史', '船山信次', 950, 9784121019745);
+INSERT INTO texts(category_id, book, name, price, isbn, states) VALUES(9, '工学基礎', '沖野亮太', 2100, 4297474742321);
+INSERT INTO texts(category_id, book, name, price, isbn, states) VALUES(10, '食品の科学', '渡部徹', 2350, 9779035711221);
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,81 @@
+-- 各種テーブル削除
+DROP TABLE IF EXISTS admin;
+DROP TABLE IF EXISTS categories;
+DROP TABLE IF EXISTS states;
+DROP TABLE IF EXISTS texts;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS cards;
+DROP TABLE IF EXISTS order_details;
+DROP TABLE IF EXISTS ordered_items;
+
+-- 管理者テーブル
+CREATE TABLE admin
+(
+	id SERIAL PRIMARY KEY,
+	password TEXT
+);
+-- 学部テーブル
+CREATE TABLE categories
+(
+   id SERIAL PRIMARY KEY,
+   name TEXT
+);
+-- 商品状態テーブル
+CREATE TABLE states
+(
+   id SERIAL PRIMARY KEY,
+   name TEXT
+);
+-- 書籍テーブル
+CREATE TABLE texts
+(
+   id SERIAL PRIMARY KEY,
+   category_id INTEGER,
+   book TEXT,
+   name TEXT,
+   price INTEGER,
+   isbn INTEGER,
+   state INTEGER
+);
+-- ユーザテーブル
+CREATE TABLE users
+(
+   id SERIAL PRIMARY KEY,
+   name TEXT,
+   email TEXT,
+   address TEXT,
+   tel TEXT,
+   password TEXT
+);
+-- 購入テーブル
+CREATE TABLE orders
+(
+   id SERIAL PRIMARY KEY,
+   user_id INTEGER,
+   ordered_on DATE,
+   total_price INTEGER
+);
+-- クレジットカードテーブル
+CREATE TABLE cards
+(
+	id SERIAL PRIMARY KEY,
+	number INTEGER,
+	name TEXT,
+	date INTEGER,
+	cord INTEGER
+);
+-- 購入明細テーブル
+CREATE TABLE order_details
+(
+   id SERIAL PRIMARY KEY,
+   order_id INTEGER,
+   text_id INTEGER,
+   quantity INTEGER
+);
+-- 購入履歴テーブル
+CREATE TABLE ordered_items
+(
+	id SERIAL PRIMARY KEY,
+	order_detail_id INTEGER
+);
