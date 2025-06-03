05fb0bf09bc8fbe062b6b041eb3b78ce3f123909
aiueo <aiueo@aaa.com>
2025-06-03 13:14:21 +0900

    0603 LoginCommit

---
+++ spring-kaihatu1/src/main/java/com/example/demo/SpringKaihatu1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKaihatu1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKaihatu1Application.class, args);
+	}
+
+}
